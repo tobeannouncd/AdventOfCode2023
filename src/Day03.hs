c9f2dba2819eb0c7a0524f8c15767437f11a274f
tobeannouncd <wzrds3@gmail.com>
2023-12-03 15:21:41 -0500

    Day03

--- src/Day03.hs
+++ src/Day03.hs
@@ -1,7 +1,57 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
 module Day03 (solve) where
 
-import qualified Data.Text as T
+import Data.Text (Text, unpack)
+import Control.Arrow ( Arrow((&&&)) )
+import Data.Char ( isDigit )
+import Data.List (intersect)
 
-solve :: T.Text -> ((),())
-solve = undefined
+
+solve :: Text -> (Int,Int)
+solve = (part1 &&& part2) . lines . unpack
+
+
+(!) :: [[a]] -> (Int,Int) -> a
+xss ! (i,j) = xss !! i !! j
+
+part1 :: [String] -> Int
+part1 grid = sum
+    [ num
+    | (pts,num) <- findNums grid
+    , any (isSym . (grid !)) (adjAll pts)]
+  where
+    isSym x = x `notElem` ('.':['0'..'9'])
+    adjAll pts = [p | pt <- pts, p <- adj pt, p `notElem` pts]
+    adj (i,j) = [(i+di,j+dj) | di <- [-1 .. 1], dj <- [-1 .. 1], (di,dj) /= (0,0),
+      i+di >= 0, i+di < rows, j+dj >= 0, j+dj < cols]
+    rows = length grid
+    cols = length (head grid)
+
+    
+findNums :: [String] -> [([(Int,Int)],Int)]
+findNums grid = 
+    [ (map (i,) js, num) 
+    | (i,row) <- zip [0..] grid
+    , (js, num) <- findNums' $ zip [0..] row ]
+  where
+    findNums' [] = []
+    findNums' xs@((_,x):_)
+      | isDigit x = let (a,b) = span (isDigit.snd) xs
+                        (is, ns) = unzip a
+                        in (is, read ns):findNums' b
+      | otherwise = findNums' $ dropWhile (not . isDigit . snd) xs
+
+
+part2 :: [String] -> Int
+part2 grid = sum gears
+  where
+    ipts = findNums grid
+    adj (i,j) = [(ii,jj) | ii <- [i-1 .. i+1], jj <- [j-1 .. j+1]
+      , (ii,jj) /= (i,j)]
+    gears =
+      [ product (map snd xs)
+      | (i,row) <- zip [0..] grid
+      , (j,'*') <- zip [0..] row
+      , let adj' = adj (i,j)
+      , let xs = filter (\(pts,_) -> not $ null $ intersect pts adj') ipts
+      , length xs == 2]
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Test.Hspec ( hspec, describe, it, shouldBe )
 import Day01 (solve)
+import Day02 (solve)
+import Day03 (solve)
 
 main :: IO ()
 main = hspec $ do
@@ -30,4 +32,34 @@ main = hspec $ do
         
         in mapM_ test cases
 
+  describe "Day 2" $ do
+    let example =
+          "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\n\
+          \Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\n\
+          \Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\n\
+          \Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\n\
+          \Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
 
+        (ans1, ans2) = Day02.solve example
+
+    it "Part 1 matches the sample" $ do
+      ans1 `shouldBe` 8
+    it "Part 2 matches the sample" $ do
+      ans2 `shouldBe` 2286
+
+  describe "Day 3" $ do
+    let example =
+          "467..114..\n\
+          \...*......\n\
+          \..35..633.\n\
+          \......#...\n\
+          \617*......\n\
+          \.....+.58.\n\
+          \..592.....\n\
+          \......755.\n\
+          \...$.*....\n\
+          \.664.598.."
+
+    it "Part 1" $ do
+      fst (Day03.solve example) `shouldBe` 4361
+      
