ddf3cfeb3354c039cf2b2908d8a793c2b96e8abc
tobeannouncd <wzrds3@gmail.com>
2023-12-05 22:06:08 -0500

    added days 4,5

--- src/Day04.hs
+++ src/Day04.hs
@@ -2,6 +2,40 @@
 module Day04 (solve) where
 
 import qualified Data.Text as T
+import Data.Text.Read (decimal)
+import Data.Bits (Bits(shiftL))
+import Control.Arrow ((&&&))
 
-solve :: T.Text -> ((),())
-solve = undefined
+data Card = Card
+  { winningNums :: [Int]
+  , pickedNums :: [Int]
+  } deriving (Show,Eq)
+
+solve :: T.Text -> (Int,Int)
+solve = (part1 &&& part2) . map parseCard . T.lines
+
+parseCard :: T.Text -> Int
+parseCard t
+  | [_, t'] <- T.splitOn ": " t
+  , [a, b] <- T.splitOn " | " t'
+  , Right ws <- traverse decimal (T.words a)
+  , Right ns <- traverse decimal (T.words b)
+  = let chosen = map fst ws :: [Int]
+        picked = map fst ns
+        in length . filter (`elem` picked) $ chosen
+parseCard _ = error "cannot parse"
+
+
+part1 :: [Int] -> Int
+part1 = sum . map score
+  where
+    score 0 = 0
+    score n = shiftL 1 (n-1)
+
+part2 :: [Int] -> Int
+part2 = go (f [])
+  where
+    go (x:xs) (m:ms) =
+      1 + x + go (zipWith (+) xs $ f (replicate m (1+x))) ms
+    go _ _ = 0
+    f = (++ repeat 0)
--- src/Day05.hs
+++ src/Day05.hs
@@ -2,6 +2,58 @@
 module Day05 (solve) where
 
 import qualified Data.Text as T
+import Control.Arrow (Arrow(..))
+import Data.Text.Read (decimal)
 
-solve :: T.Text -> ((),())
-solve = undefined
+data MapItem
+  = MapItem
+  { dst :: Int
+  , src :: Int
+  , len :: Int
+  } deriving (Show,Eq)
+
+solve :: T.Text -> (Int,Int)
+solve input = (part1 maps &&& part2 maps) seeds
+  where
+    (seeds, maps) = parse input
+
+parse :: Integral b => T.Text -> ([b], [[MapItem]])
+parse input
+  | (a:b) <- T.splitOn "\n\n" input
+  , (_:xs) <- T.words a
+  , Right seeds <- traverse (fmap fst . decimal) xs
+  = (seeds, map (map parseItem . tail . T.lines) b)
+parse _ = error "cannot parse"
+
+parseItem :: T.Text -> MapItem
+parseItem line
+  | Right [a,b,c] <- traverse (fmap fst . decimal) (T.words line)
+  = MapItem a b c
+parseItem _ = error "cannot parse"
+
+part1 :: Foldable t => t [MapItem] -> [Int] -> Int
+part1 maps = minimum . map (\x -> foldl mapRange x maps)
+
+mapRange :: Int -> [MapItem] -> Int
+mapRange x [] = x
+mapRange x (MapItem d s l:xs)
+  | s <= x && x < s + l = d + x - s
+  | otherwise = mapRange x xs
+
+part2 :: Foldable t => t [MapItem] -> [Int] -> Int
+part2 maps seeds = fst . minimum . foldl f (toPairs seeds) $ maps
+  where
+    f xs ms = xs >>= (`mapRange'` ms)
+    toPairs (x:y:xs) = (x,y) : toPairs xs
+    toPairs _ = []
+
+mapRange' :: (Int, Int) -> [MapItem] -> [(Int, Int)]
+mapRange' x [] = [x]
+mapRange' (rs,rl) (MapItem d s l:ms)
+  | rs <= s + l && s < rs + rl = pre ++ cur ++ suf
+  | otherwise = mapRange' (rs,rl) ms
+  where
+    pre = if rs < s then mapRange' (rs,s-rs) ms else []
+    cur = [(d + max 0 (rs-s), min rl (l - max 0 (rs-s)))]
+    suf = if s+l < rs + rl then mapRange' (s+l, rs+rl-s-l) ms else []
+
